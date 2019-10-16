8aa674ffb3e71e1350e415f1cfe702c3fe8b6f09
Mikalai Sokarau <mikalaisokarau@gmail.com>
2019-10-16 20:57:27 +0300

    add course component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
+import { CourseComponent } from './components/course/course.component';
 import { CoursesComponent } from './pages/courses/courses.component';
 import { FakeLogoComponent } from './components/fake-logo/fake-logo.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -13,6 +14,7 @@ import { HeaderComponent } from './components/header/header.component';
   declarations: [
     AppComponent,
     BreadcrumbsComponent,
+    CourseComponent,
     CoursesComponent,
     FakeLogoComponent,
     FooterComponent,
