15b8d3f256182f27a60e691218a7b73b78892656
Mikalai Sokarau <mikalaisokarau@gmail.com>
2019-10-16 20:46:17 +0300

    register components in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
+import { CoursesComponent } from './pages/courses/courses.component';
+import { FakeLogoComponent } from './components/fake-logo/fake-logo.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BreadcrumbsComponent,
+    CoursesComponent,
+    FakeLogoComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
