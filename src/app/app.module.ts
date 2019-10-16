ac16813a31ad5c495178cfed07647fec9cb76412
Mikalai Sokarau <mikalaisokarau@gmail.com>
2019-10-16 21:00:22 +0300

    add control panel component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
+import { ControlPanelComponent } from './components/control-panel/control-panel.component';
 import { CourseComponent } from './components/course/course.component';
 import { CoursesComponent } from './pages/courses/courses.component';
 import { FakeLogoComponent } from './components/fake-logo/fake-logo.component';
@@ -14,11 +15,12 @@ import { HeaderComponent } from './components/header/header.component';
   declarations: [
     AppComponent,
     BreadcrumbsComponent,
+    ControlPanelComponent,
     CourseComponent,
     CoursesComponent,
     FakeLogoComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
