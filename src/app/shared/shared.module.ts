35850839064e2e7591f567653dc2b51a5ad42e42
Mikalai Sokarau <mikalaisokarau@gmail.com>
2019-10-27 12:09:29 +0300

    move footer and header to shared components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { FakeLogoComponent } from './components/fake-logo/fake-logo.component';
 import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
+import { FakeLogoComponent } from './components/fake-logo/fake-logo.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
+    BreadcrumbsComponent,
     FakeLogoComponent,
-    BreadcrumbsComponent
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
+    BreadcrumbsComponent,
     FakeLogoComponent,
-    BreadcrumbsComponent
+    FooterComponent,
+    HeaderComponent
   ]
 })
 export class SharedModule { }
