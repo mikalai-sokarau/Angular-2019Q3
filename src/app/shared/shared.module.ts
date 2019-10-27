5057720dee0eb175d39277d8d2b7bb603427e47d
Mikalai Sokarau <mikalaisokarau@gmail.com>
2019-10-27 12:12:01 +0300

    rename fake-logo-component to logo-component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
-import { FakeLogoComponent } from './components/fake-logo/fake-logo.component';
+import { LogoComponent } from './components/logo/logo.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     BreadcrumbsComponent,
-    FakeLogoComponent,
+    LogoComponent,
     FooterComponent,
     HeaderComponent
   ],
@@ -18,7 +18,7 @@ import { HeaderComponent } from './components/header/header.component';
   ],
   exports: [
     BreadcrumbsComponent,
-    FakeLogoComponent,
+    LogoComponent,
     FooterComponent,
     HeaderComponent
   ]
