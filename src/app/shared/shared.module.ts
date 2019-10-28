de80ec1d353288a538b6b535aac0a06cd98611cb
Mikalai Sokarau <mikalaisokarau@gmail.com>
2019-10-28 20:36:00 +0300

    course styling

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -8,9 +8,14 @@ import {
   faEdit,
   faEllipsisH,
   faTrashAlt,
+  faUserGraduate,
   IconDefinition
 } from '@fortawesome/free-solid-svg-icons';
-import { faPlayCircle } from '@fortawesome/free-regular-svg-icons';
+import {
+  faCalendarAlt,
+  faClock,
+  faPlayCircle
+} from '@fortawesome/free-regular-svg-icons';
 
 import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
 import { LogoComponent } from './components/logo/logo.component';
@@ -19,11 +24,14 @@ import { HeaderComponent } from './components/header/header.component';
 
 const icons: Array<IconDefinition> = [
   faBars,
+  faClock,
+  faCalendarAlt,
   faEdit,
   faEllipsisH,
   faPlayCircle,
   faSortDown,
-  faTrashAlt
+  faTrashAlt,
+  faUserGraduate
 ];
 
 @NgModule({
