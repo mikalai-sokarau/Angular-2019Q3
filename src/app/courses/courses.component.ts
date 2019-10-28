426929afe8d65990e09f274268b5f0fad3d58aef
Mikalai Sokarau <mikalaisokarau@gmail.com>
2019-10-28 21:55:16 +0300

    add lifecycle hooks to courses component

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy
+} from '@angular/core';
 import { ICourse } from './course/course.model';
 import { HttpClient } from '@angular/common/http';
 
@@ -7,7 +17,15 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
-export class CoursesComponent implements OnInit {
+export class CoursesComponent implements
+  OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy {
   courses: Array<ICourse> = [];
   private readonly coursesPath = '../../assets/mock-data/courses-data.json';
 
@@ -15,13 +33,43 @@ export class CoursesComponent implements OnInit {
 
   ngOnInit() {
     this.http.get<Array<ICourse>>(this.coursesPath)
-      .subscribe((courses: Array<ICourse>) => {
-        courses.length = 5;
-        this.courses = courses;
-      });
+    .subscribe((courses: Array<ICourse>) => {
+      courses.length = 5;
+      this.courses = courses;
+    });
   }
 
   onDeleteCourse(id: string) {
     console.log(`Delete: ${id}`);
   }
+
+  // tslint:disable no-conflicting-lifecycle
+  ngOnChanges() {
+    console.log('ngOnChanges');
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck');
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked');
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked');
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy');
+  }
+  // tslint:enable no-conflicting-lifecycle
 }
