91e2f63b24fc5c05f483e36894a01b313c10fe66
Mikalai Sokarau <mikalaisokarau@gmail.com>
2019-11-11 21:46:29 +0300

    move pipes to directories

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -5,10 +5,10 @@ import { ControlPanelComponent } from './components/control-panel/control-panel.
 import { CourseComponent } from './components/course/course.component';
 import { CourseHighlightingDirective } from './directives/course-highlighting.directive';
 import { CoursesComponent } from './courses.component';
-import { DurationPipe } from './pipes/duration-pipe.pipe';
+import { DurationPipe } from './pipes/duration/duration-pipe.pipe';
 import { FilterPipe } from './pipes/filter/filter.pipe';
 import { LoadMoreComponent } from './components/load-more/load-more.component';
-import { OrderByPipe } from './pipes/order-by.pipe';
+import { OrderByPipe } from './pipes/orderBy/order-by.pipe';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
