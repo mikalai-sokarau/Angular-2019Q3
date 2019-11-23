4b4aadbc2ee47120a86108d511e96dd20761dec8
Mikalai Sokarau <Mikalai_Sokarau@epam.com>
2019-11-23 11:29:50 +0300

    bind form inputs to the login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  public userEmail: string;
+  public userPassword: string;
 
   constructor(public authService: AuthService) { }
 
