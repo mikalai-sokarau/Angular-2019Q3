b437c733a223f6c81b87d9b2e94c0a052b857bef
Mikalai Sokarau <Mikalai_Sokarau@epam.com>
2019-12-02 23:20:27 +0300

    add guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,13 @@ import { CoursesComponent } from './pages/courses/courses.component';
 import { AddCourseComponent } from './pages/courses/components/add-course/add-course.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { EditCourseComponent } from './pages/courses/components/edit-course/edit-course.component';
+import { AuthGuard } from './core/guards/auth-guard.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: '/courses', pathMatch: 'full' },
-  { path: 'courses', component: CoursesComponent, pathMatch: 'full' },
-  { path: 'courses/new', component: AddCourseComponent, pathMatch: 'full' },
-  { path: 'courses/:id', component: EditCourseComponent, pathMatch: 'full' },
+  { path: 'courses', component: CoursesComponent, pathMatch: 'full', canActivate: [AuthGuard] },
+  { path: 'courses/new', component: AddCourseComponent, pathMatch: 'full', canActivate: [AuthGuard] },
+  { path: 'courses/:id', component: EditCourseComponent, pathMatch: 'full', canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent, pathMatch: 'full' },
   { path: '**', component: NotFoundComponent, pathMatch: 'full' },
 ];
