24bba8c73b3e2b100a175ccc906d569fbb258b97
Mikalai Sokarau <Mikalai_Sokarau@epam.com>
2019-12-02 23:08:23 +0300

    add breadcrumbs

--- src/app/shared/components/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/components/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { IBreadcrumb } from './breadcrumbs.model';
+import { ActivatedRoute } from '@angular/router';
+import { CoursesService } from 'src/app/pages/courses/services/courses/courses.service';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./breadcrumbs.component.scss']
 })
 export class BreadcrumbsComponent implements OnInit {
+  private readonly DEFAULT_BREADCRUMB: IBreadcrumb = { name: 'courses', link: '/courses' };
+  public breadcrumbs: Array<IBreadcrumb> = [this.DEFAULT_BREADCRUMB];
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private coursesService: CoursesService
+  ) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      try {
+        const id = params.get('id');
+        const course = this.coursesService.getCourseById(id);
+        const breadcrumb = { name: course.title, link: '' };
+        
+        this.breadcrumbs.push(breadcrumb);
+      } catch (e) { /* do nothing */ }
+    })
   }
 
 }
