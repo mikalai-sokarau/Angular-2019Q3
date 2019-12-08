c2adef763f72d7d98c50c73bb003511f4595ef64
Mikalai Sokarau <Mikalai_Sokarau@epam.com>
2019-12-08 18:04:23 +0300

    save user info to localstorage

--- src/app/core/services/auth/auth.service.ts
+++ src/app/core/services/auth/auth.service.ts
@@ -10,12 +10,14 @@ import { CoursesService } from 'src/app/pages/courses/services/courses/courses.s
 export class AuthService {
   public isAuthenticated: boolean;
   private readonly loginUrl = '/api/login';
+  private readonly userStoreId = 'user';
   private user: IUser | null;
 
   constructor(
     private router: Router,
     private http: HttpClient
     ) {
+    this.user = this.getUserFromStore();
     this.isAuthenticated = Boolean(this.user);
   }
 
@@ -30,6 +32,7 @@ export class AuthService {
         if (userData) {
           this.user = userData;
           this.isAuthenticated = true;
+          this.saveUserToStore(userData);
           this.router.navigate(
             ['/courses'],
             { queryParams: { size: CoursesService.DEFAULT_COURSES_SIZE } }
@@ -41,10 +44,27 @@ export class AuthService {
   public logout(): void {
     this.user = null;
     this.isAuthenticated = false;
+    this.clearUserData();
     this.router.navigate(['/login']);
   }
 
   public getUserInfo(): IUser {
     return this.user;
+  }
+
+  private getUserFromStore(): IUser {
+    const user = localStorage.getItem(this.userStoreId);
+    
+    return user
+      ? JSON.parse(user)
+      : null;
+  }
+
+  private clearUserData(): void {
+    localStorage.removeItem(this.userStoreId);
+  }
+
+  private saveUserToStore(user: IUser): void {
+    localStorage.setItem(this.userStoreId, JSON.stringify(user));
   }
 }
