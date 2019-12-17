6492c74a954f597af8cb296a6be0ae3829949ee1
Mikalai Sokarau <Mikalai_Sokarau@epam.com>
2019-12-17 23:39:53 +0300

    add debounce + filter to search

--- src/app/pages/courses/components/control-panel/control-panel.component.ts
+++ src/app/pages/courses/components/control-panel/control-panel.component.ts
@@ -1,22 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
 import { Router, Params } from '@angular/router';
+import { fromEvent, interval, Subscription, Observable } from 'rxjs';
+import { debounce, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-control-panel',
   templateUrl: './control-panel.component.html',
   styleUrls: ['./control-panel.component.scss']
 })
-export class ControlPanelComponent implements OnInit {
+export class ControlPanelComponent implements AfterViewInit, OnDestroy {
   public searchText = '';
+  public searchEvent$: Observable<string>;
+  private subscription: Subscription;
+  private DEBOUNCE_INTERVAL_MS = 300;
+  
+  @ViewChild('searchInput', { static: false }) searchInput;
 
   constructor(private router: Router) {}
 
-  ngOnInit() {
+  ngAfterViewInit(): void {
+    this.searchEvent$ = fromEvent<string>(this.searchInput.nativeElement, 'keyup')
+      .pipe(
+        filter(() => this.searchText.length >= 3),
+        debounce(() => interval(this.DEBOUNCE_INTERVAL_MS))
+      );
+
+    this.subscription = this.searchEvent$.subscribe(() => this.doSearch());
   }
 
-  public onSearchClick(): void {
+  private doSearch(): void {
+    console.log(this.searchText);
+    
     const queryParams: Params = { find: this.searchText };
 
     this.router.navigate([], { queryParams });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
